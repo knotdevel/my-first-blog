5c29ce3198a2f69df6c928f382e549a52cafd7f8
Knot Devel <knotdevel@users.noreply.github.com>
2019-11-21 18:06:02 +0900

    Changed DEBUG to False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9w1nqzu+=g4+qc+8b4olza^0lbg_j1*z+_8*5&b7oign-%esc6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
